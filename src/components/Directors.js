dc61a5eb8fe40e8c0dd726a36cad6515e0923ffe
KarinaStabile <karina.stabile@gmail.com>
2022-02-20 16:50:46 -0500

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  let dir = directors.map(d => (
+    <div key={d.name}>
+      <h1>{d.name}</h1>
+      <ul>{d.movies.map((movie) => (
+        <li key={movie}>{movie}</li>
+      ))}</ul>
+    </div>
+  ))
+  return (
+  <div>
+    <h1>Directors Page</h1>
+      {dir}
+      </div>
+      );
 }
-
 export default Directors;
